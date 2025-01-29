748aa69e18834949cee7d7902f1e173ba95d49e8
Aldo Caceres <aldocj34@gmail.com>
2025-01-28 21:05:36 -0500

    Database stuff
    Just adding some stuff relating to the database

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -1,2 +1,16 @@
 # This is where create functions that will make requests to the vlrggapi to retrieve data
 # and then store that data in the database.
+
+import requests
+import json
+
+url = "https://vlrggapi.vercel.app/stats?region=na&timespan=30"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+
+    with open("matches_data.json", "w") as file:
+        json.dump(data, file, indent=4)
+    print("Data fetched successfully")
+else:
+    print(f"Error: {response.status_code}")
